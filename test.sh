935437ec7f92cfec0b89d57a8ed901fba5a31a51
Antonio Terceiro <terceiro@debian.org>
2014-09-20 10:30:44 -0300

    test.sh: extract command running

--- test.sh
+++ test.sh
@@ -2,95 +2,95 @@
 
 test_Testsuite_autopkgtest_pkg_ruby() {
   has debian/control 'Testsuite: autopkgtest-pkg-ruby'
-  run autodep8
+  check_run autodep8
 }
 
 test_XS_Testsuite_autopkgtest_pkg_ruby() {
   has debian/control 'XS-Testsuite: autopkgtest-pkg-ruby'
-  run autodep8
+  check_run autodep8
 }
 
 test_Testsuite_autopkgtest_pkg_perl() {
   has debian/control 'Testsuite: autopkgtest-pkg-perl'
-  run autodep8
+  check_run autodep8
 }
 
 test_XS_Testsuite_autopkgtest_pkg_perl() {
   has debian/control 'XS-Testsuite: autopkgtest-pkg-perl'
-  run autodep8
+  check_run autodep8
 }
 
 test_Testsuite_autopkgtest_pkg_nodejs() {
   has debian/control 'Testsuite: autopkgtest-pkg-nodejs'
-  run autodep8
+  check_run autodep8
 }
 
 test_XS_Testsuite_autopkgtest_pkg_nodejs() {
   has debian/control 'XS-Testsuite: autopkgtest-pkg-nodejs'
-  run autodep8
+  check_run autodep8
 }
 
 test_ruby_rake() {
   has debian/ruby-tests.rake
-  run autodep8
+  check_run autodep8
 }
 
 test_ruby_testrb() {
   has debian/ruby-tests.rb
-  run autodep8
+  check_run autodep8
 }
 
 test_ruby_test_files() {
   has debian/ruby-test-files.yaml
-  run autodep8
+  check_run autodep8
 }
 
 test_ruby_no_debhelper() {
   has debian/ruby-tests.rake
   has debian/control "Build-Depends: debhelper, gem2deb, rake"
-  run autodep8
+  check_run autodep8
   assertFalse "test does must not depend on debhelper\n$(grep Depends: stdout)\n" "grep -q Depends:.*debhelper stdout"
 }
 
 test_ruby_no_gem2deb() {
   has debian/ruby-tests.rake
   has debian/control "Build-Depends: debhelper, gem2deb, rake"
-  run autodep8
+  check_run autodep8
   assertFalse "test does must not depend on gemdeb\n$(grep Depends: stdout)\n" "grep -q 'Depends:.*gem2deb(\s\|,)' stdout"
 }
 
 test_ruby_gem2deb_test_runner() {
   has debian/ruby-tests.rb
-  run autodep8
+  check_run autodep8
   assertTrue "test depends on gem2deb-test-runner\n$(grep Depends: stdout)\n" 'grep Depends:.*gem2deb-test-runner stdout'
 }
 
 test_perl_Makefile_PL() {
   has_dir t
   has Makefile.PL
-  run autodep8
+  check_run autodep8
 }
 
 test_perl_Build_PL() {
   has_dir t
   has Build.PL
-  run autodep8
+  check_run autodep8
 }
 
 test_perl_test_pl() {
   has test.pl
   has Build.PL
-  run autodep8
+  check_run autodep8
 }
 
 test_detect_nodejs() {
   has 'debian/control' 'Source: node-foo'
-  run autodep8
+  check_run autodep8
 }
 
 test_nodejs_upstream_name() {
   has 'debian/control' 'Source: node-foo'
-  run autodep8
+  check_run autodep8
   assertTrue 'get the upstream from node package' 'grep --quiet "require.*foo" stdout'
   assertFalse 'does not include node- prefix' 'grep --quiet node-foo stdout'
 }
@@ -104,7 +104,11 @@ fi
 run() {
   exitstatus=0
   "$@" > stdout 2> stderr || exitstatus=$?
-  assertEquals "$1 failed!\n$(tail -n 10000 stderr stdout;)\n" 0 "$exitstatus"
+}
+
+check_run() {
+  run "$@"
+  assertEquals "$@ failed!\n$(tail -n 10000 stderr stdout;)\n" 0 "$exitstatus"
 }
 
 has() {
