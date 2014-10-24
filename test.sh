770b973c1595718009018a76db9eb4b31aac7e03
Jean-Baptiste Lallement <jeanbaptiste.lallement@gmail.com>
2014-10-24 12:30:14 -0400

    support/dkms: Add support for dkms packages
    This needs a corresponding "dkms-autopkgtest" script in DKMS itself, see
    <https://bugs.debian.org/769095>.
    
    Closes: #766668

--- test.sh
+++ test.sh
@@ -35,6 +35,16 @@ test_XS_Testsuite_autopkgtest_pkg_nodejs() {
   check_run autodep8
 }
 
+test_Testsuite_autopkgtest_pkg_dkms() {
+  has debian/control 'Testsuite: autopkgtest-pkg-dkms'
+  check_run autodep8
+}
+
+test_XS_Testsuite_autopkgtest_pkg_dkms() {
+  has debian/control 'XS-Testsuite: autopkgtest-pkg-dkms'
+  check_run autodep8
+}
+
 test_ruby_rake() {
   has debian/ruby-tests.rake
   check_run autodep8
@@ -99,6 +109,33 @@ test_nodejs_upstream_name() {
   assertTrue 'get the upstream from node package' 'grep --quiet "require.*foo" stdout'
   assertFalse 'does not include node- prefix' 'grep --quiet node-foo stdout'
 }
+
+test_detect_dkms_depends() {
+  has 'debian/control' 'Depends: dkms'
+  check_run autodep8
+}
+
+test_detect_dkms_builddepends() {
+  has 'debian/control' 'Build-Depends: dkms'
+  check_run autodep8
+}
+
+test_detect_dkms_builddepends_indep() {
+  has 'debian/control' 'Build-Depends-Indep: dkms'
+  check_run autodep8
+}
+
+test_detect_dkms_packagename() {
+  has 'debian/control' 'Package: foo-dkms'
+  check_run autodep8
+}
+
+test_dkms_depends_dkms() {
+  has 'debian/control' "Depends: dkms"
+  check_run autodep8
+  assertFalse "test does must depend on dkms\n$(grep Depends: stdout)\n" "grep -q Depends:.*depends stdout"
+}
+
 
 ##################################################
 
