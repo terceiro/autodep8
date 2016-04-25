e6f51bc7484b200f04d6a4ae6576338057b7cfd9
Antonio Terceiro <terceiro@debian.org>
2016-04-25 16:13:32 -0300

    avoid partial matches in `Testsuite` field
    e.g. `r` should never match `ruby`

--- test/r_test.sh
+++ test/r_test.sh
@@ -13,4 +13,11 @@ test_upstream_name_from_description() {
   assertTrue 'get package from DESCRIPTION' 'grep --quiet "library.*FOO" stdout'
 }
 
+test_should_not_try_on_other_package_types_starting_with_r() {
+  has 'debian/control' 'Testsuite: autopkgtest-pkg-rrrrrrr'
+  run autodep8
+  assertEquals 1 "$exitstatus"
+  assertEquals "" "$(cat stdout stderr)"
+}
+
 . shunit2
