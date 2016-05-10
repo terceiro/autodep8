1480b3b4b6ddfc8844ed0087e440a9a88df5add1
Sean Whitton <spwhitton@spwhitton.name>
2016-05-10 13:27:13 -0700

    add test suite for elpa test detection

---
+++ test/elpa_test.sh
@@ -0,0 +1,40 @@
+. $(dirname $0)/helper.sh
+
+test_Testsuite_autopkgtest_pkg_elpa() {
+  has debian/control 'Testsuite: autopkgtest-pkg-elpa'
+  has debian/rules 'foo'
+  check_run autodep8
+}
+
+test_XS_Testsuite_autopkgtest_pkg_elpa() {
+  has debian/control 'XS-Testsuite: autopkgtest-pkg-elpa'
+  has debian/rules 'foo'
+  check_run autodep8
+}
+
+test_elpa_test_ert() {
+  has debian/control 'Build-Depends: dh-elpa'
+  has debian/rules 'foo'
+  has test.el '(ert-deftest foo)'
+  check_run autodep8
+}
+
+test_elpa_test_buttercup() {
+  has debian/control 'Build-Depends: dh-elpa, elpa-buttercup'
+  has debian/rules 'foo'
+  check_run autodep8
+}
+
+test_elpa_test_disabled() {
+  has debian/control 'Build-Depends: dh-elpa'
+  has debian/rules 'export DH_ELPA_TEST_DISABLE'
+  check_fail autodep8
+}
+
+test_elpa_test_non_elpa() {
+  has debian/control 'Build-Depends: dh-fake-elpa'
+  has debian/rules 'foo'
+  check_fail autodep8
+}
+
+. shunit2
