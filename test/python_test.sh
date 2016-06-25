f5e46abd8b9801589aabd6cdab85752b31aae13c
Ondřej Nový <onovy@debian.org>
2016-06-26 00:05:58 +0200

    Python: Run import test on all Python versions

--- test/python_test.sh
+++ test/python_test.sh
@@ -4,16 +4,16 @@ test_python_detect_source_py2() {
   has 'debian/control' 'Source: python-foo\n\nPackage:python-foo'
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
-  assertTrue 'have py2 test' 'grep --quiet "python -c" stdout'
-  assertFalse 'dont have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertTrue 'have py2 test' 'grep --quiet "pyversions" stdout'
+  assertFalse 'dont have py3 test' 'grep --quiet "py3versions" stdout'
 }
 
 test_python_detect_source_py3() {
   has 'debian/control' 'Source: python-foo\n\nPackage:python3-foo'
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
-  assertFalse 'dont have py2 test' 'grep --quiet "python -c" stdout'
-  assertTrue 'have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertFalse 'dont have py2 test' 'grep --quiet "pyversions" stdout'
+  assertTrue 'have py3 test' 'grep --quiet "py3versions" stdout'
 }
 
 # PyPy is only Python 2 compatible for now.
@@ -22,32 +22,32 @@ test_python_detect_source_pypy() {
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
   assertTrue 'have pypy test' 'grep --quiet "pypy -c" stdout'
-  assertFalse 'dont have py2 test' 'grep --quiet "python -c" stdout'
-  assertFalse 'dont have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertFalse 'dont have py2 test' 'grep --quiet "pyversions" stdout'
+  assertFalse 'dont have py3 test' 'grep --quiet "py3versions" stdout'
 }
 
 test_python_detect_binary_py2() {
   has 'debian/control' 'Source: foo\n\nPackage: python-foo'
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
-  assertTrue 'have py2 test' 'grep --quiet "python -c" stdout'
-  assertFalse 'dont have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertTrue 'have py2 test' 'grep --quiet "pyversions" stdout'
+  assertFalse 'dont have py3 test' 'grep --quiet "py3versions" stdout'
 }
 
 test_python_detect_binary_py3() {
   has 'debian/control' 'Source: foo\n\nPackage: python3-foo'
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
-  assertFalse 'dont have py2 test' 'grep --quiet "python -c" stdout'
-  assertTrue 'have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertFalse 'dont have py2 test' 'grep --quiet "pyversions" stdout'
+  assertTrue 'have py3 test' 'grep --quiet "py3versions" stdout'
 }
 
 test_python_detect_binary_pypy() {
   has 'debian/control' 'Source: foo\n\nPackage: pypy-foo'
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
-  assertFalse 'dont have py2 test' 'grep --quiet "python -c" stdout'
-  assertFalse 'dont have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertFalse 'dont have py2 test' 'grep --quiet "pyversions" stdout'
+  assertFalse 'dont have py3 test' 'grep --quiet "py3versions" stdout'
   assertTrue 'have pypy test' 'grep --quiet "pypy -c" stdout'
 }
 
@@ -55,8 +55,8 @@ test_python_detect_binary_all() {
   has 'debian/control' 'Source: foo\n\nPackage: python-foo\n\nPackage: python3-foo\n\nPackage: pypy-foo'
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
-  assertTrue 'have py2 test' 'grep --quiet "python -c" stdout'
-  assertTrue 'have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertTrue 'have py2 test' 'grep --quiet "pyversions" stdout'
+  assertTrue 'have py3 test' 'grep --quiet "py3versions" stdout'
   assertTrue 'have pypy test' 'grep --quiet "pypy -c" stdout'
 }
 
@@ -64,24 +64,24 @@ test_python_ignore_doc_py2() {
   has 'debian/control' 'Source: foo\n\nPackage: python-foo-doc\n\nPackage: python-foo'
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
-  assertTrue 'have py2 test' 'grep --quiet "python -c" stdout'
-  assertFalse 'dont have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertTrue 'have py2 test' 'grep --quiet "pyversions" stdout'
+  assertFalse 'dont have py3 test' 'grep --quiet "py3versions" stdout'
 }
 
 test_python_ignore_doc_py3() {
   has 'debian/control' 'Source: foo\n\nPackage: python3-foo-doc\n\nPackage: python3-foo'
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
-  assertFalse 'dont have py2 test' 'grep --quiet "python -c" stdout'
-  assertTrue 'have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertFalse 'dont have py2 test' 'grep --quiet "pyversions" stdout'
+  assertTrue 'have py3 test' 'grep --quiet "py3versions" stdout'
 }
 
 test_python_ignore_doc_pypy() {
   has 'debian/control' 'Source: foo\n\nPackage: pypy-foo-doc\n\nPackage: pypy-foo'
   check_run autodep8
   assertTrue 'get upstream name' 'grep --quiet "import foo;" stdout'
-  assertFalse 'dont have py2 test' 'grep --quiet "python -c" stdout'
-  assertFalse 'dont have py3 test' 'grep --quiet "python3 -c" stdout'
+  assertFalse 'dont have py2 test' 'grep --quiet "pyversions" stdout'
+  assertFalse 'dont have py3 test' 'grep --quiet "py3versions" stdout'
   assertTrue 'have pypy test' 'grep --quiet "pypy -c" stdout'
 }
 
