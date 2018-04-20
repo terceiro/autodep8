ee35db1c70ea72c696188293030e4e8e5a4da9b2
Paul Gevers <elbrus@debian.org>
2018-04-20 20:24:29 +0200

    Drop obsolete part of the test for commit 3f4cb53

--- test/r_test.sh
+++ test/r_test.sh
@@ -10,7 +10,6 @@ test_upstream_name_from_description() {
   has 'debian/control' 'Source: r-cran-foo'
   has 'DESCRIPTION' 'Package: FOO'
   check_run autodep8
-  assertTrue 'get package from DESCRIPTION' 'grep --quiet "library.*FOO" stdout'
 }
 
 test_should_not_try_on_other_package_types_starting_with_r() {
