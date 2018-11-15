7471d7eba926028dd7be0124754000c493fd8f10
Antonio Terceiro <terceiro@debian.org>
2018-11-15 12:43:21 -0200

    Handle the case where debian/tests/control.autodep8 has no trailing newline.

--- test/generic_test.sh
+++ test/generic_test.sh
@@ -13,4 +13,13 @@ test_append_to_control_autodep8() {
   assertTrue 'empty line after contents of debian/tests/control.autodep8' "grep '^\$' stdout"
 }
 
+test_append_to_control_autodep8_with_no_trailing_newline() {
+  has debian/control 'Testsuite: autopkgtest-pkg-ruby'
+  has debian/tests/control.autodep8
+  printf "Test: foo" > debian/tests/control.autodep8 # no trailing newline!
+  run autodep8
+  stanzas="$(grep-dctrl -c -F Test,Test-Command '' stdout)"
+  assertEquals "expected 2 test stanzas, found $stanzas" 2 "$stanzas"
+}
+
 . shunit2
